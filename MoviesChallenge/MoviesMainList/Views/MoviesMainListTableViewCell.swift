db291ec68106123a3b088b60e2faeddcabafe9b7
Safa Ads <safa.ads@itworx.com>
2019-09-06 14:59:32 +0200

    Project configurations and structure set

---
+++ MoviesChallenge/MoviesMainList/Views/MoviesMainListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesMainListTableViewCell.swift
+//  MoviesChallenge
+//
+//  Created by Safa Ads on 9/6/19.
+//  Copyright © 2019 safaads. All rights reserved.
+//
+
+import UIKit
+
+class MoviesMainListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
