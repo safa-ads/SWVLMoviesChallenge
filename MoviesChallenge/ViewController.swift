8b8404efaa80027bbc977fd87ea8a1b3f7dddc55
Safa Ads <safa.ads@itworx.com>
2019-09-06 14:45:16 +0200

    Initial Commit

---
+++ MoviesChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesChallenge
+//
+//  Created by Safa Ads on 9/6/19.
+//  Copyright © 2019 safaads. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
